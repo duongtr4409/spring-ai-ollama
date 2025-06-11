4cef988a302e7d0538425d40aea66a7a91fe49ce
DuowngTora Mac <nvduong216@gmail.com Mac>
2025-06-11 22:20:13 +0700

    UpdateConfig - update/cors-config - add allowed origins to properties file

--- src/main/java/com/duowngtora/spring_ollama/config/CorsConfiguration.java
+++ src/main/java/com/duowngtora/spring_ollama/config/CorsConfiguration.java
@@ -1,23 +1,40 @@
 package com.duowngtora.spring_ollama.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
+    @Value("${application.cors.allowed-origins:}")
+    private String allowedOrigins;
+
+    private List<String> getAllowedOrigins(){
+        if(allowedOrigins.isEmpty()) return new ArrayList<>();
+        return Arrays.stream(allowedOrigins.split(",")).map(String::trim).toList();
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // Áp dụng cho tất cả endpoints
-            .allowedOrigins(
-                    "https://trusted-domain.com",
-                    "http://localhost:5173"
-            )
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+
+
+        CorsRegistration corsRegistration = registry.addMapping("/**");// Áp dụng cho tất cả endpoints
+
+        List<String> allowedOrigins = this.getAllowedOrigins();
+        if(allowedOrigins.isEmpty()){
+            corsRegistration.allowedOrigins("*");
+        }else{
+            for(String origin : allowedOrigins)
+                corsRegistration.allowedOrigins(origin);
+        }
+        corsRegistration.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .exposedHeaders("Authorization", "Content-Disposition")
             .allowCredentials(true)
